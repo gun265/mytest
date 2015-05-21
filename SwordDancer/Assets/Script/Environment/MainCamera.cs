bfb005eee4bc9facccb6635eb9a8c492730e1153
gunwoo <chan8709@naver.com>
2015-05-22 07:33:00 +0900

    SwordDancer 추가

---
+++ SwordDancer/Assets/Script/Environment/MainCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+    public GameObject Player;
+    // XYZ 증가값
+    public float plusX;
+    public float plusY;
+    public float plusZ;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        transform.position = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 TempVec = transform.position;
+
+        // 실시간으로 쿼터뷰 카메라 위치 변경
+        TempVec.x = Player.transform.position.x - plusX;
+        TempVec.y = Player.transform.position.y + plusY;
+        TempVec.z = Player.transform.position.z - plusZ;
+
+        // 위치갱신
+        transform.position = TempVec;
+
+	}
+}
---
+++ SwordDancer/Assets/Script/Player/CharactorControl.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharactorControl : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+    public float rotationSpeed = 1.0f;
+
+    Vector3 moveVector;
+    Vector3 rotateVector;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //float amtMove = moveSpeed * Time.smoothDeltaTime;
+        //float amtRotaion = rotationSpeed * Time.smoothDeltaTime;
+
+        // 전방벡터 변수
+        //Vector3 forwardVec = transform.forward;
+        
+
+        // 키보드 조작 부분
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(forwardVec * amtMove, Space.World);
+        //    this.animation.CrossFade("Hero_Walk");
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    //transform.Translate(-1 * Vector3.right * amtMove, Space.World);
+        //    transform.Rotate(Vector3.up, -amtRotaion);
+        //    this.animation.CrossFade("Hero_Walk");
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(-1 * forwardVec * amtMove, Space.World);
+        //    this.animation.CrossFade("Hero_Walk");
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    //transform.Translate(Vector3.right * amtMove, Space.World);
+        //    transform.Rotate(Vector3.up, amtRotaion);
+        //    this.animation.CrossFade("Hero_Walk");
+        //}
+        float horizontalkey = Input.GetAxis("Horizontal");
+        float verticalkey = Input.GetAxis("Vertical");
+
+        playerMove(horizontalkey, verticalkey);   
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.animation.CrossFade("Hero_Attack");
+        }
+	}
+
+    void playerMove(float h, float v)
+    {
+        this.transform.Translate(this.transform.forward * v * moveSpeed * Time.smoothDeltaTime, Space.World);
+        this.transform.Rotate(transform.up, h * rotationSpeed * Time.smoothDeltaTime);
+        this.animation.CrossFade("Hero_Walk");
+    }
+
+    void onAniEnd()
+    {
+        this.animation.CrossFade("Hero_Wait");
+    }
+}
